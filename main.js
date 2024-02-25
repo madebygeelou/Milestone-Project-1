942bf717f1c6a9ebca7b383038a10d6fd410b077
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-02-25 11:55:52 -0800

    made changes to the whole page

--- main.js
+++ main.js
@@ -20,12 +20,12 @@
 function clickGreen() {
     var sound = new Audio('158914__carlmartin__cajun-kick-hit-19.wav');
     sound.play();
-    debugger
+    document.body.style.backgroundColor="#6886ff";
     let button = document.getElementById('button1');
     button.classList.add("green-clicked");
         setTimeout(function() {
             button.classList.remove("green-clicked");
-        }, 1000); // Change 1000 to the desired duration in milliseconds
+        }, 100); // Change 1000 to the desired duration in milliseconds
 }
 
 document.addEventListener("keydown", e => {
@@ -38,12 +38,14 @@ document.addEventListener("keydown", e => {
  });
 
  function clickRed() {
-    var sound = document.getElementById('audio2');
+    var sound = new Audio('678742__cloud-10__martin-garrix-animals-drop-pluck-original.wav');
     sound.play();
-
+    document.body.style.backgroundColor="#ffa268";
+    let button = document.getElementById('button2');
+    button.classList.add("red-clicked");
         setTimeout(function() {
-            button.classList.remove("button1-clicked");
-        }, 1000); // Change 1000 to the desired duration in milliseconds
+            button.classList.remove("red-clicked");
+        }, 100);
 }
 document.addEventListener("keydown", e => {
  e.preventDefault();
@@ -55,13 +57,14 @@ document.addEventListener("keydown", e => {
  });
 
  function clickYellow() {
-    var sound = document.getElementById('audio3');
+    var sound = new Audio('680995__cloud-10__m1-classic-organ.wav');
     sound.play();
-
-//         setTimeout(function() {
-//             button.classList.remove("button1-clicked");
-//         }, 1000); // Change 1000 to the desired duration in milliseconds
-// }
+    document.body.style.backgroundColor="#cdffb2";
+    let button = document.getElementById('button3');
+    button.classList.add("yellow-clicked");
+        setTimeout(function() {
+            button.classList.remove("yellow-clicked");
+        }, 100);
 }
 document.addEventListener("keydown", e => {
  e.preventDefault();
@@ -73,13 +76,14 @@ document.addEventListener("keydown", e => {
  });
 
  function clickBlue() {
-    var sound = document.getElementById('audio4');
+    var sound = new Audio('563387__badoink__woop.wav');
     sound.play();
-
-//         setTimeout(function() {
-//             button.classList.remove("button1-clicked");
-//         }, 1000); // Change 1000 to the desired duration in milliseconds
-// }
+    document.body.style.backgroundColor="#ff86b6";
+    let button = document.getElementById('button4');
+    button.classList.add("blue-clicked");
+        setTimeout(function() {
+            button.classList.remove("blue-clicked");
+        }, 100);
 }
 document.addEventListener("keydown", e => {
  e.preventDefault();
