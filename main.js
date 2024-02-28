4b64b807447264626f15e70c019639300424ac77
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-02-27 20:31:08 -0800

    made final tweaks to the game

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ function greenGradient(){
 function clickGreen() {
     var sound = new Audio('158914__carlmartin__cajun-kick-hit-19.wav');
     sound.play();
-    document.body.style.color="blue";
+    document.body.className='';
+    document.body.classList.add('green-gradient');
     let button = document.getElementById('button1');
     button.classList.add("green-clicked");
         setTimeout(function() {
@@ -28,7 +29,8 @@ document.addEventListener("keydown", e => {
  function clickRed() {
     var sound = new Audio('678742__cloud-10__martin-garrix-animals-drop-pluck-original.wav');
     sound.play();
-    document.body.style.backgroundColor="#ffa268";
+    document.body.className='';
+    document.body.classList.add('red-gradient');
     let button = document.getElementById('button2');
     button.classList.add("red-clicked");
         setTimeout(function() {
@@ -50,7 +52,8 @@ document.addEventListener("keydown", e => {
  function clickYellow() {
     var sound = new Audio('680995__cloud-10__m1-classic-organ.wav');
     sound.play();
-    document.body.style.backgroundColor="#cdffb2";
+    document.body.className='';
+    document.body.classList.add('yellow-gradient');
     let button = document.getElementById('button3');
     button.classList.add("yellow-clicked");
         setTimeout(function() {
@@ -72,7 +75,8 @@ document.addEventListener("keydown", e => {
  function clickBlue() {
     var sound = new Audio('563387__badoink__woop.wav');
     sound.play();
-    document.body.style.backgroundColor="#ff86b6";
+    document.body.className='';
+    document.body.classList.add('blue-gradient');
     let button = document.getElementById('button4');
     button.classList.add("blue-clicked");
         setTimeout(function() {
